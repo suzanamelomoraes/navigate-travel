9c3118f0b63967594fe73406823d2718fdcb692e
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-02-17 09:38:17 +1300

    update products component to receive products data as props from dates component and render the details

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
-function Products() {
-  return <div></div>;
-}
+const Products = ({ products }) => {
+  return (
+    <div>
+      {products.map(product => (
+        <div>
+          <div>
+            <p>{product.class}</p>
+            <div style={{ border: "solid 1px red" }}>
+              <p>{product.status}</p>
+              <p style={{ textDecoration: "line-through" }}>{product.price}</p>
+              <p>{product.priceWithDiscount}</p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Products;
