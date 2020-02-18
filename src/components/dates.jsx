5c378482be34d533e0ea1bd0e2e529ce4a4b7b70
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-02-19 12:23:32 +1300

    create Container styled component to display the details in a row with the text at the center and also temperature emphasized by a red blackground

--- src/components/dates.jsx
+++ src/components/dates.jsx
@@ -1,24 +1,36 @@
 import React from "react";
+import styled from "styled-components"
 import Products from "./products";
 
+const Container = styled.div`
+display: flex;
+flex-flow: row nowrap;
+text-align: center;
+padding: 20px;
+`;
 
+const P = styled.p`
+background: red;
+border-radius: 20px;
+color: white;
+`;
 
 
 const Dates = ({ dates }) => {
   return (
-    <div className="container">
+    <Container>
       {dates.map(date => (
         <div>
           <h5>{date.date}</h5>
           <h6>{date.dateText}</h6>
-          <h6>{date.dateDay}</h6>
-          <p>{date.temperature}</p>
+          <h6>({date.dateDay})</h6>
+          <P>{date.temperature}</P>
           <div>
           <Products products={date.products} />
           </div>
         </div>
       ))}
-    </div>
+    </Container>
   );
 };
 
