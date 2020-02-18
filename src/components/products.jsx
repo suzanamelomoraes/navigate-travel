9bb4d3357de30c39255dc5635f9afa9f0c48bec6
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-02-19 12:33:31 +1300

    create a container styled-components to add a border to the product details

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,19 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  border: solid 1px;
+  border-color: darkgray;
+  border-radius: 5px;
+`;
 
 const Products = ({ products }) => {
   return (
     <div>
       {products.map(product => (
-        <div>
-          <div>
-            <p>{product.class}</p>
-            <div style={{ border: "solid 1px red" }}>
-              <p>{product.status}</p>
-              <p style={{ textDecoration: "line-through" }}>{product.price}</p>
-              <p>{product.priceWithDiscount}</p>
-            </div>
-          </div>
-        </div>
+        <Container>
+          <p>{product.status}</p>
+          <p style={{ textDecoration: "line-through" }}>{product.price}</p>
+          <p>{product.priceWithDiscount}</p>
+        </Container>
       ))}
     </div>
   );
