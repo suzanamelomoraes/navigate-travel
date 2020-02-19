694acbfc15d0e8c5413e6942a87b4600249de454
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-02-19 14:38:00 +1300

    delete unecessary divs

--- src/components/dates.jsx
+++ src/components/dates.jsx
@@ -1,20 +1,19 @@
 import React from "react";
-import styled from "styled-components"
+import styled from "styled-components";
 import Products from "./products";
 
 const Container = styled.div`
-display: flex;
-flex-flow: row nowrap;
-text-align: center;
-padding: 20px;
+  display: flex;
+  flex-flow: row nowrap;
+  text-align: center;
+  padding: 20px;
 `;
 
 const P = styled.p`
-background: red;
-border-radius: 20px;
-color: white;
+  background: red;
+  border-radius: 20px;
+  color: white;
 `;
-
 
 const Dates = ({ dates }) => {
   return (
@@ -25,9 +24,8 @@ const Dates = ({ dates }) => {
           <h6>{date.dateText}</h6>
           <h6>({date.dateDay})</h6>
           <P>{date.temperature}</P>
-          <div>
+
           <Products products={date.products} />
-          </div>
         </div>
       ))}
     </Container>
