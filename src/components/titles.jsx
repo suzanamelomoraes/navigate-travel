1a8b9e4ceddb4eb82334aa804bd227be5edd2cf9
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-02-19 15:38:51 +1300

    create styled components to style the titles

--- src/components/titles.jsx
+++ src/components/titles.jsx
@@ -1,5 +1,5 @@
-import React from "react"
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
 
 const Title = styled.div`
   display: flex;
@@ -7,15 +7,35 @@ const Title = styled.div`
   justify-content: space-around;
 `;
 
+const T1 = styled.h1`
+  color: red;
+  font-style: oblique;
+`;
+
+const T2 = styled.h1`
+  color: red;
+  font-style: oblique;
+`;
+
+const T3 = styled.h2``;
+
+const T4 = styled.h2``;
+
+const T5 = styled.h2``;
+
+
 const Titles = () => {
-return(
+  return (
     <Title>
-    <h1>Our Prices</h1>
-    <h2>Premier</h2>
-    <h2>Premier Plus</h2>
-    <h2>Catamaran</h2>
-  </Title>
-)
-}
+      <div>
+        <T1>Our</T1>
+        <T2>Prices</T2>
+      </div>
+      <T3>Premier</T3>
+      <T4>Premier Plus</T4>
+      <T5>Catamaran</T5>
+    </Title>
+  );
+};
 
 export default Titles;
